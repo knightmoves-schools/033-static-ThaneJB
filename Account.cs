00e4dd6404ef0f1b9d71a20a52e0522328019df9
Thane <thabay0606@students.maquoketaschools.org>
2026-04-08 14:12:42 -0500

    Maybe?

--- Account.cs
+++ Account.cs
@@ -2,5 +2,16 @@
 
 public class Account {
    // Add your code here
+   public static string Name = "";
+
+   public static ChangeName(string newName){
+      Name = newName;
+
+      return Name;
+   }
+
+   static MainAccount(){
+      Name = "Main Account";
+   }
 }
 
